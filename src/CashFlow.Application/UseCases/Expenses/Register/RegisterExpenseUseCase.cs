421bac572af580aea9367b0340b7357f4100e18a
Leandro Marqueti <107411587+leandromarqueti@users.noreply.github.com>
2026-02-09 23:36:54 -0300

    removendo cache

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,7 +7,26 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var TitleIsEmpty = string.IsNullOrEmpty(request.Title);
+
+        if (TitleIsEmpty)
+            throw new ArgumentException("Title is required.");
+        if (request.Amount <= 0)
+            throw new ArgumentException("Amount must be greater than zero.");
+        //var resultData = DateTime.Compare(request.Date, DateTime.UtcNow);
+        //if (resultData > 0)
+        //    throw new ArgumentException("Date cannot be in the future.");
+        //ou
+        if (request.Date > DateTime.Now)
+            throw new ArgumentException("Date cannot be in the future.");
+        if (!Enum.IsDefined(typeof(PaymentType), request.PaymentType))
+            throw new ArgumentException("Invalid payment type.");
     }
 }
