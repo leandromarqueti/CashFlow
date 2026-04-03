1a0c9344d2f4c8b34de83daf16775fc3f2b02307
Leandro Marqueti <107411587+leandromarqueti@users.noreply.github.com>
2026-04-03 20:58:53 -0300

    Commit dos testes

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Validators.Tests.Expenses.Register;   
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Expense Title",
+            Amount = 100,
+            Description = "Description",
+            Date = DateTime.Now.AddDays(-1),
+            PaymentType = PaymentType.CreditCard
+        };
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}   
