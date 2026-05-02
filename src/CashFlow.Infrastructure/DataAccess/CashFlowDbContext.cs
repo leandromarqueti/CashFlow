f4b8677b225920c379e299b5f88b75cbd40acf0c
Leandro Marqueti <107411587+leandromarqueti@users.noreply.github.com>
2026-05-02 13:37:45 -0300

    Adicionando banco de dados MySql

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CashFlow.Domain.Entities; 
+
+namespace CashFlow.Infrastructure.DataAccess;
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses{ get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflow_db;Uid=root;Pwd=@password123;";
+        var version = new Version(8, 0, 45);
+        var serverVersion = new MySqlServerVersion(version);
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
