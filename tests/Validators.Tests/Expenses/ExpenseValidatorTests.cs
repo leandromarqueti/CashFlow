c57645caa25ffac7ea4067ad0446970a5a22a8db
Leandro Marqueti <107411587+leandromarqueti@users.noreply.github.com>
2026-06-21 01:10:45 -0300

    Add Validators tests

--- tests/Validators.Tests/Expenses/ExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/ExpenseValidatorTests.cs
@@ -4,8 +4,8 @@ using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
-namespace Validators.Tests.Expenses.Register;
-public class RegisterExpenseValidatorTests
+namespace Validators.Tests.Expenses;
+public class ExpenseValidatorTests
 {
     [Fact]
     public void Success()
@@ -22,7 +22,7 @@ public class RegisterExpenseValidatorTests
         //    PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
         //};
 
-        var request = RequestRegisterExpenseJsonBuilder.Build();
+        var request = RequestExpenseJsonBuilder.Build();
 
         //Act
         var result = validator.Validate(request);
@@ -33,14 +33,17 @@ public class RegisterExpenseValidatorTests
         result.IsValid.Should().BeTrue();
     }
 
-    [Fact]
-    public void Error_TitleEmpty()
+    [Theory]
+    [InlineData("")]
+    [InlineData("         ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
     {
         //Arrange
         var validator = new ExpenseValidator();
 
-        var request = RequestRegisterExpenseJsonBuilder.Build();
-        //request.Title = string.Empty;
+        var request = RequestExpenseJsonBuilder.Build();
+        request.Title = title;
 
         //Act
         var result = validator.Validate(request);
@@ -56,8 +59,8 @@ public class RegisterExpenseValidatorTests
         //Arrange
         var validator = new ExpenseValidator();
 
-        var request = RequestRegisterExpenseJsonBuilder.Build();
-        //request.Date = DateTime.UtcNow.AddDays(1);
+        var request = RequestExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
 
         //Act
         var result = validator.Validate(request);
@@ -73,7 +76,7 @@ public class RegisterExpenseValidatorTests
         //Arrange
         var validator = new ExpenseValidator();
 
-        var request = RequestRegisterExpenseJsonBuilder.Build();
+        var request = RequestExpenseJsonBuilder.Build();
         //request.PaymentType = (PaymentType)700;
 
         //Act
@@ -87,13 +90,15 @@ public class RegisterExpenseValidatorTests
     [Theory]
     [InlineData(0)]
     [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
     public void Error_Amount_Invalid(decimal amount)
     {
         //Arrange
         var validator = new ExpenseValidator();
 
-        var request = RequestRegisterExpenseJsonBuilder.Build();
-        //request.Amount = amount;
+        var request = RequestExpenseJsonBuilder.Build();
+        request.Amount = amount;
 
         //Act
         var result = validator.Validate(request);
@@ -101,6 +106,22 @@ public class RegisterExpenseValidatorTests
         //Assert
         result.IsValid.Should().BeFalse();
         result.Errors.Should().ContainSingle().And.Contain(err => err.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
+
+    [Fact]
+    public void Error_Tag_Invalid()
+    {
+        //Arrange
+        var validator = new ExpenseValidator();
+        var request = RequestExpenseJsonBuilder.Build();
+        request.Tags.Add((Tag)1000);
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TAG_TYPE_NOT_SUPPORTED));
     }
 
 }
