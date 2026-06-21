08cd458eefe4a43bf29c3a449ce537930338c336
Leandro Marqueti <107411587+leandromarqueti@users.noreply.github.com>
2026-06-21 01:09:30 -0300

    Add CommonTestUtilities

---
+++ tests/CommonTestUtilities/Requests/RequestExpenseJsonBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestExpenseJsonBuilder
+{
+    public static RequestExpenseJson Build()
+    {
+        //return new RequestRegisterExpenseJson
+        //{
+        //    Amount = 100,
+        //    Description = string.Empty,
+        //    Title = "Test",
+        //    Date = DateTime.Now.AddDays(-1),
+        //    PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        //};
+        return new Faker<RequestExpenseJson>()
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000))
+            .RuleFor(r => r.Tags, faker => faker.Make(1, () => faker.PickRandom<Tag>()));
+
+    }
+}
